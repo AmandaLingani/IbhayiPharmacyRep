fcb11fcf4b60b45e2f11dece2775dc712b7e80dc
Amanda Lingani <157322732+AmandaLingani@users.noreply.github.com>
2025-12-18 22:57:53 +0200

    Add files via upload

---
+++ DoctorRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrescribingSystem.Data;
+using PrescribingSystem.Interfaces;
+using PrescribingSystem.Models;
+using PrescribingSystem.Models.ViewModels;
+
+namespace PrescribingSystem.Repositories
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DoctorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddDoctor(DoctorViewModel model)
+        {
+            var doctor = new Doctor
+            {
+                DoctorId = model.DoctorId,
+                Name = model.DoctorFirstName,
+                Surname = model.DoctorLastName,
+                Practice_Number = model.PracticeNumber,
+                Email = model.Email
+            };
+
+            _context.Doctor.Add(doctor);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ PrescriptionRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrescribingSystem.Data;
+using PrescribingSystem.Interfaces;
+using PrescribingSystem.Models;
+using PrescribingSystem.Models.ViewModels;
+
+namespace PrescribingSystem.Repositories
+{
+    public class PrescriptionRepository : IPrescriptionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrescriptionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<IGrouping<DateTime, Prescription>>> GetCustomerUploadedPrescriptionsAsync()
+        {
+            return (await _context.Prescriptions
+                 .Include(p => p.Customer)
+                 .Where(p => p.Source == "CustomerUploaded" && p.PrescriptionStatus == PrescriptionStatus.Pending)
+                 .OrderByDescending(p => p.PrescriptionDate.Date)
+                 .ToListAsync())
+                 .GroupBy(p => p.PrescriptionDate.Date);
+        }
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsByCustomerAsync(string customerId)
+        {
+            return await _context.Prescriptions
+                .Include(p => p.Customer)
+                .Include(p => p.Doctor)
+                .Where(p => p.CustomerId == customerId)
+                .OrderByDescending(p => p.PrescriptionDate)
+                .ToListAsync();
+        }
+        public async Task<Prescription?> GetPrescriptionByIdAsync(int prescriptionId)
+        {
+            return await _context.Prescriptions
+                .Include(p => p.Customer)
+                .Include(p => p.Doctor)
+                .Include(p => p.MedicationItems)
+                .ThenInclude(mp => mp.Medication)
+                .FirstOrDefaultAsync(p => p.PrescriptionId == prescriptionId);
+        }
+        public async Task AddPrescriptionAsync(Prescription prescription)
+        {
+            _context.Prescriptions.Add(prescription);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdatePrescriptionAsync(Prescription prescription)
+        {
+            _context.Prescriptions.Update(prescription);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<Doctor>> GetDoctorsAsync()
+        {
+            return await _context.Doctor
+                .ToListAsync();
+        }
+        public async Task<IEnumerable<Medication>> GetMedicationsAsync()
+        {
+            return await _context.Medication
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<MedicationItem>> GetMedicationsForProcessingAsync(int prescriptionId)
+        {
+            var prescriptions = await _context.Prescriptions
+                .Include(p => p.MedicationItems)
+                .ThenInclude(mp => mp.Medication)
+                .FirstOrDefaultAsync(p => p.PrescriptionId == prescriptionId);
+
+            if (prescriptions == null)
+                return Enumerable.Empty<MedicationItem>();
+
+            if (prescriptions.MedicationItems.All(m => m.TotalRepeats == null || m.TotalRepeats == 5))
+                return prescriptions.MedicationItems;
+
+            return prescriptions.MedicationItems
+                .Where(m => m.TotalRepeats > 0)
+                .ToList();
+        }
+      
+
+    }
+
+}
